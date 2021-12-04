f3f5c4e1f37d3ba900103d7bf71dc5998e42093c
akoncsugit <aastadle@ncsu.edu>
2021-12-03 23:35:45 -0500

    Switching data set due to errors with model fitting of previous data set

--- roughdraft.R
+++ roughdraft.R
@@ -1,7 +1,11 @@
 library(tidyverse)
-library(corrplot)
-library(lares)
-library(stats)
+library(corrplot) 
+library(naniar)
+# library(logistf)
+
+# library(lares)
+# library(stats)
+
 ObesityDataSet <- read.csv("ObesityDataSet_raw_and_data_sinthetic.csv") %>%
   mutate(Weight_Level = as_factor(NObeyesdad), Gender = as_factor(Gender), 
          Family_History = as_factor(family_history_with_overweight),
@@ -61,10 +65,67 @@ corrplot()
 g <- ggplot(data = ObesityDataSet, aes(x= Weight_Level)) + 
   xlab("Diagnosis by Gender") + ylab("Density")
 
-# Filled, stacked histogram with density of shares by weekend level
-g + geom_count(aes(x = Weight_Level, fill = Gender)), postion = dodge) 
 
-  labs(title = "Density of Shares: Weekday vs. Weekend") + 
-  scale_fill_discrete(name = "Weekday or Weekend?",
-                      labels = c("Weekday", "Weekend"))
-corrplot::corrplot(O)
+
+################################ KEEP THIS CODE ################################
+
+g <- ggplot(ObesityDataSet, aes(as.factor(Diagnosis)))
+
+## one discrete variable
+d <- g + geom_bar() 
+d + theme_light() 
+d + theme_minimal()
+
+
+# One diescrete filled by another
+df <- ggplot(ObesityDataSet, aes(as.factor(Diagnosis), fill = Family_History)) 
+df + geom_bar(position = "dodge")
+
+## two discreate variables
+d2 <- ggplot(ObesityDataSet, aes(as.factor(Diagnosis), Gender))
+d2 + geom_count(aes(shape = Gender))
+
+
+## continous one variable
+c <- ggplot(ObesityDataSet, aes(Weight))
+c + geom_histogram(binwidth = 5)
+
+## two continous variables
+c2 <- ggplot(ObesityDataSet, aes(Weight, Age))
+
+## filled by diagnosis
+c2 + geom_jitter(height = 2, width = 2, aes(color = factor(Diagnosis), alpha = Height))
+## alpha is opacity, color, shape, size options
+
+
+
+
+### one discrete and one continous variable
+dc <- ggplot(ObesityDataSet, aes(as.factor(Diagnosis), Age))
+
+## Family History
+dc + geom_boxplot(aes(fill = as.factor(Diagnosis))) + facet_wrap(~Family_History)
+
+ObFitData <- ObesityDataSet %>% select(1:16, 18) %>% 
+  mutate(Diagnosis = as_factor(Diagnosis))
+fit <- glm(Diagnosis ~ ., data = ObFitData, family = "binomial")
+fit
+## remove intercept term with -1
+summary(fit)
+
+
+#Plot of Quality vs Total sulfur dioxide
+data <- glmPlot %>% group_by(`total sulfur dioxide`) %>% summarise(propGood = mean(qualityBin), n = n())
+ggplot(data, aes(x = `total sulfur dioxide`, y = propGood, size = n)) + geom_point(stat = "identity")+
+  ggtitle("Quality vs Total sulfur dioxide")
+
+
+
+glmFit1 <- 
+
+
+
+data <- ObFitData %>% filter(Diagnosis == 0)
+
+
+
